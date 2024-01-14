aef4e011479d5cae463fc9cbb3880622a2b1d4a7
yoswos <151448407+yoswosyotelc@users.noreply.github.com>
2024-01-14 19:01:42 +0300

    Sololearn.c

--- Sololearn.c
+++ Sololearn.c
@@ -1,31 +1,316 @@
-// @#$!@#$@!#$Progress### << ///Dec_2023..??>> @ @#$@%#!@%!Saving??@!#$@!#$@!#$!@#$@!#$
-// !!!!! don't stay at one part for more than 1 weekds...!!!!
-// !!!!! proceed as you go through by commenting .. for furhter refirinement..
-// //..//...//..//...//..//..//..//...//
-#include <iostream>
-using namespace std;
+//@#$!@#$@!#$Progress### << ///Jan_2023..??>> @ @#$@%#!@%!Saving??@!#$@!#$@!#$!@#$@!#$
+//!!!!! don't stay at one part for more than 1 weekds...!!!!
+//!!!!! proceed as you go through by commenting .. for furhter refirinement..
+//..//...//..//...//..//..//..//...//
+//..//..Code explanation.//..use...//.ChatGPT.//..//..//...//
+//..//...//..//...//..//..//..//...//
 
 
-
-
-
+/*________Codecoach Challenges__________________ */
 
 /*
-#include <iostream>
-using namespace std;
-
-int main()
-{
-	cout << "Hello world cpp" << endl;
-	return 0;
-}
 
 
-int main()
-{	// declaring and input output okay.. using cpp 
-	int name;
-	cin>> name;
-	cout<<name <<endl;
-}
 
-//what is list for cpp .. why?? it has created a .exe.. file.. there ?
+
+
+*/
+
+
+/*Intro_C_______________ Basic Concepts _________________*/
+
+// /*
+// _____________ Getting started with C
+
+// #include <stdio.h>
+// int main(){
+	// printf("Iam lenaring c");
+	// return 0;
+// }
+
+// _____________ Statements
+
+// #include <stdio.h>
+// int main(){
+	// printf("Welcome to C");
+	// return 0;
+// }
+
+// _____________ Program Structure
+
+// #include <stdio.h>	// header files
+// int main(){
+	
+	// printf("First");
+	// printf("Second");
+	// return 0;
+// }
+
+// _____________ Output Formatting
+
+//______Output
+
+// #include <stdio.h>
+// int main(){
+	
+	// printf(122);
+	// return 0;
+// }
+
+//______ Numbers
+
+// #include <stdio.h>
+// int main(){
+	// printf("%d",253); // double
+	// return 0;
+// }
+
+// #include <stdio.h>
+// int main(){
+	// printf("%f",3.14); // float
+	// return 0;
+	
+// }
+
+// #include <stdio.h>
+// int main(){
+	// printf("%c",'X'); // string
+	// return 0;
+// }
+
+// #include <stdio.h>
+// int main(){
+	// printf("%d%d%c",3,2,'z'); // combinaiton
+	// return 0;
+// }
+
+// #include <stdio.h>
+// int main(){ 
+	// printf("Name: %s","james");	// mixing it up
+	// printf("Age: %d",42); // mixing it up
+	// return 0;
+// }
+
+// #include <stdio.h>
+
+// int main() {
+    // printf("Name: %s\n", "James");
+    // printf("Age: %d", 42);
+    // return 0;
+// }
+
+// #include <stdio.h>
+// int main (){ 
+	// printf("%c\n%c\n%c",'A','B','C');
+	// return 0;
+// }
+
+
+// #include <stdio.h>	// product Labels
+
+// int main() {
+    // printf("Product: %s \n", "LCD Monitor");
+    // printf("Price: %lf", 49.95);
+    // return 0;
+// }
+
+//_________________ Variables
+
+// #include <stdio.h>
+// int main (){ 
+	// int score;
+	// score=0;
+	// printf("%d",score);
+	// return 0;
+// }
+
+
+// #include <stdio.h>
+// int main (){ 
+	// int score=3.4; // see output here..
+	// printf("%d",score);
+	// return 0;
+// }
+
+
+// #include <stdio.h>
+// int main() {
+    // int score = 253;
+    // score = 280;
+    // printf("%d", score);
+    // return 0;
+// }
+
+// #include <stdio.h>
+// int main(){ 
+	// int x=9;
+	// int y=8;
+	// x=3;
+	// printf("%d%d",y,x);	
+	// return 0;
+// }
+
+
+// #include <stdio.h> // What is the price?
+// int main() {
+    // int price = 12; 
+    // int symbol = '$';    
+    // printf("Price: %c%d",symbol, price);
+    // return 0;
+// }
+
+
+//_________________ Data Types
+//______ Output
+//_________________ Doing Math
+//______ Output
+//_________________ Comments
+//______ Output
+
+
+// */
+
+
+
+/*Intro_C_______________ Conditinals and Loops _________________*/
+
+/*
+
+//_________________ Numbers
+//______ Output
+//_________________ Numbers
+//______ Output
+//_________________ Numbers
+//______ Output
+//_________________ Numbers
+//______ Output
+//_________________ Numbers
+//______ Output
+
+
+
+*/
+
+
+/*Intro_C_______________ Arrays & Strings _________________*/
+
+/*
+
+//_________________ Numbers
+//______ Output
+//_________________ Numbers
+//______ Output
+//_________________ Numbers
+//______ Output
+//_________________ Numbers
+//______ Output
+//_________________ Numbers
+//______ Output
+
+
+
+
+*/
+
+
+/*Intro_C_______________ Functions & Pointers _________________*/
+
+/*
+
+//_________________ Numbers
+//______ Output
+//_________________ Numbers
+//______ Output
+//_________________ Numbers
+//______ Output
+//_________________ Numbers
+//______ Output
+//_________________ Numbers
+//______ Output
+
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
